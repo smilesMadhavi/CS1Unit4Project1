d3316271ce7ac53f39e169b979aebd16955142a4
smilesMadhavi <72471453+smilesMadhavi@users.noreply.github.com>
2020-11-12 09:32:04 -0600

    Add files via upload

---
+++ MuseumTicket.java
@@ -0,0 +1,50 @@
+
+/**
+ * Write a description of class MuseumTicket here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MuseumTicket
+{
+    // instance variables - replace the example below with your own
+    private String Day;
+    private int age ;
+    private double price;
+    private String coupon;
+    
+
+    /**
+     * Constructor for objects of class MuseumTicket
+     */
+    public MuseumTicket()
+    {
+        // initialise instance variables
+        
+    }
+    public MuseumTicket( String myDay, int myAge, String myCoupon)
+    {
+        
+        
+    }
+    
+    public void setTickets ( String myDay, int myAge, String myCoupon)
+    {
+        
+        
+    }
+   
+    public void calculatePrice()
+    {
+      // if statements to decide assign price to the ticket
+        
+    }
+    public double getPrice()
+    {
+        return price;
+    }
+    public void printTicket()
+    {
+        // print the ticket  
+    }
+}
---
+++ MuseumTicketRunner.java
@@ -0,0 +1,43 @@
+
+/**
+ * Write a description of class MuseumTicketRunner here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+// 1.  import Scanner 
+public class MuseumTicketRunner
+{
+    public static void main(String[] args)
+    {
+        // instantiate an object of Class Scanner
+        
+        // create a variable of totalPrice
+        
+        // instantiate ticket Object using default constructor
+        
+        // ask the user for how many tickets
+        // save the value in a variable numTickets of type int
+        
+        
+        // write a for loop 
+        
+        
+            // ask the user to input  day , age and discount coupon
+            
+            
+            // call the method setTicket() with arguments day, age , discount
+            
+            
+            // call method printTicket()
+            
+            
+            // call method calculatePrice() 
+           
+            // call getPrice() and add the value to totalPrice
+            
+         // print total ticket price
+            
+        
+    }
+}
